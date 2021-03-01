3cc3033da69b6d680cc59f54e6d3ac581254e20a
JUNGQUI <ljk2518@gmail.com>
2021-03-01 21:48:36 +0900

    create-react-app development 외 다수

--- cra-test/src/index.js
+++ cra-test/src/index.js
@@ -15,3 +15,20 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// env prop
+// process.env.{변수이름}
+// process.env.NODE_ENV
+/**
+ * 실행에 따라 process.env.NODE_ENV 의 값 변화
+ * npm start : development
+ * npm test : test
+ * npm run build : production
+ **/
+
+console.log('환경변수 ', process.env.NODE_ENV);
+console.log('URI ', process.env.REACT_APP_API_URI);
+console.log('Custom env ', process.env.REACT_APP_CUSTOM_ENV);
+
+// REACT_APP_API_URL
+// custom REACT_APP... 으로 시작함
