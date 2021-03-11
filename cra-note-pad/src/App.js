390ad3a8eaf02445fba1ed0fc603e3a4ec1607de
JUNGQUI <ljk2518@gmail.com>
2021-03-11 23:52:21 +0900

    hook 예제 추가
    - batch 형식
    - setSTATE_VALUE
    - unstable_batchedUpdates

--- cra-note-pad/src/App.js
+++ cra-note-pad/src/App.js
@@ -1,14 +1,36 @@
 import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   function onClick() {
-    setCount(count + 1);
-    setCount(count + 1);
+    // 실제로 두배로 값을 늘리고 싶어서 수행했지만, count 는 1만 증가된다.
+    // setCount(count + 1);
+    // setCount(count + 1);
+    setCount(value => value + 1);
+    setCount(value => value + 1);
+
+    /**
+     * 아래의 리액트 scope 외에 이벤트 등록 호출을 할 경우에도 batchedUpdates 를 사용하면
+     * `배치처럼` 작동하게 된다.
+     */
+    // ReactDOM.unstable_batchedUpdates(() => {
+    //   setCount(value => value + 1);
+    //   setCount(value => value + 1);
+    // });
   }
   console.log('render called');
+
+  /**
+   * 이와 같이 실행 시 리액트 외부에서 호출하기에 배치로 실행되지 않는다. 따라서 값이 2번 변경되는것 까진 동일하지만
+   * 값이 2번에 걸쳐 변경되기에 렌더링이 2번 실행하고 결국 로그가 두 번 찍힌다.
+   */
+  // useEffect(() => {
+  //   window.addEventListener('click', onClick);
+  //   return () => window.removeEventListener('click', onClick);
+  // });
 
   return (
       <div>
