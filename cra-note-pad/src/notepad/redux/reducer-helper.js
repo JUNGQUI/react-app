6717d490fe984dcb3bc2dd54f7e36dae54f55cc0
JUNGQUI <ljk2518@gmail.com>
2021-05-20 19:50:03 +0900

    redux - createReducer
    TODO
    
    - createAction

--- cra-note-pad/src/notepad/redux/reducer-helper.js
+++ cra-note-pad/src/notepad/redux/reducer-helper.js
@@ -1,16 +1,31 @@
+import { createReducer } from '@reduxjs/toolkit';
 import { INITIAL_STATE, INCREASE, DECREASE, EDITSTRING } from "./action-helper";
 
-export function reducer(state=INITIAL_STATE, action) {
-  switch (action.type) {
-    case INCREASE:
-      console.log(state.value);
-      return {...state, value: state.value+1};
-    case DECREASE:
-      console.log(state.value);
-      return {...state, value: state.value-1};
-    case EDITSTRING:
-      return {...state, string: action.target}
-    default:
-      return state;
+export const reducer = createReducer(INITIAL_STATE, {
+  [INCREASE]: (state, action) => {
+    console.log(`value is ${state.value}`);
+    return {...state, value: state.value + 1};
+  },
+  [DECREASE]: (state, action) => {
+    console.log(`value is ${state.value}`);
+    return {...state, value: state.value - 1};
+  },
+  [EDITSTRING]: (state, action) => {
+    return {...state, string: action.target}
   }
-}
+})
+
+// export function reducer(state=INITIAL_STATE, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       console.log(state.value);
+//       return {...state, value: state.value+1};
+//     case DECREASE:
+//       console.log(state.value);
+//       return {...state, value: state.value-1};
+//     case EDITSTRING:
+//       return {...state, string: action.target}
+//     default:
+//       return state;
+//   }
+// }
